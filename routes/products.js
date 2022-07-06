f7860c8fd7c2b7ae2390fa69b601fea79ebaca34
akshaynexapp <akshay.p@nexapp.co.in>
2022-07-06 09:32:35 +0530

    test commit

--- routes/products.js
+++ routes/products.js
@@ -8,10 +8,13 @@ const mongoose = require('mongoose');
 router.get(`/`, async (req, res) =>{
     // localhost:3000/api/v1/products?categories=2342342,234234
 
-    
+
     let filter = {};
     if(req.query.categories)
     {
+
+
+        
          filter = {category: req.query.categories.split(',')}
     }
 
